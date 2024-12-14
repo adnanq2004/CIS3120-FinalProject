3c87e78e89c4daa7068f7a2cb77900d4cc501360
Adnan <owner@adnans-mbp.mynetworksettings.com>
2024-12-14 14:48:28 -0500

    fixed an issue in child classes, apparently you don't need to add self when calling super() initialize. also decided i need to rethink how i'm going to make use of object orietned components. i decided to add analytical/comparson data in the child classes that will be called, and interact with the dataframe object to get the data only. will flesh out the details in comments of hte code

--- Materials.py
+++ Materials.py
@@ -1,7 +1,7 @@
-From Items import Items
+from Items import Items
 
 class Materials(Items):
-    def __init__(self, location, identifier, categorgy, flavortext, ):
-        super().__init__(self, location, identifier, categorgy, flavortext)
+    def __init__(self, location, identifier, category, flavortext, ):
+        super().__init__(location, identifier, category, flavortext)
         
         # 
--- Weapons.py
+++ Weapons.py
@@ -1,22 +1,17 @@
-From Items import Items
+from Items import Items
 
 class Weapons(Items):
-    def __init__(self, location, identifier, categorgy, flavortext, damage, defense, durability):
-        super().__init__(self, location, identifier, categorgy, flavortext)
+    def __init__(self, location, identifier, category, flavortext, damage, defense):
+        super().__init__(location, identifier, category, flavortext)
         
         # damage, which should give the base amount of damage
         # defense, which should give the base amount of defense
-        # duraility, which should give the base amount of max durability
         
         self._damage=damage
         self._defense=defense
-        self._durability=durability
         
     def give_damage(self):
         return self._damage
     
     def give_defense(self):
         return self._defense
-    
-    def give_durability(self):
-        return self._durability
